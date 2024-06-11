d6b682ab531877505c6985f1baaabf2a55939987
muzaffar401 <ma9400667@gmail.com>
2024-06-11 17:34:15 +0500

    TYPESCRIPT

---
+++ DAY-32/main.js
@@ -0,0 +1,9 @@
+//Day 32 Challenge
+//task 94
+//Question 94: Use the .map() method to create a new array that contains the length of each word from an array of words.
+// Defines an array with some words
+var words = ["Hello", "World", "TypeScript", "JavaScript"];
+// Uses .map() to create a new array with the length of each word
+var lengths = words.map(function (word) { return word.length; });
+console.log(lengths); // Outputs: [5, 5, 10, 10]
+// Each number in the new array represents the length of the corresponding word in the original array.
