6b1339eb09f7ff8169163847366b8f0aa6946234
muzaffar401 <ma9400667@gmail.com>
2024-06-12 22:22:33 +0500

    TYPESCRIPT

---
+++ DAY-40/main.ts
@@ -0,0 +1,43 @@
+//Day 40 Challenge
+
+//task 118
+
+// Question 118: Write a loop that logs numbers from 1 to 10 to the console.
+
+
+// This loop logs numbers from 1 to 10
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i); // Logs the current value of i
+//   }
+//   // Each iteration increases the value of i by 1, logging it until it reaches 10.
+
+
+//task 119
+
+// Question 119: Create a while loop that logs "Hello, World!" 5 times.
+
+
+// // Initializes a counter
+// let count: number = 0;
+// // This while loop runs until count is 5
+// while (count < 5) {
+//   console.log("Hello, World!"); // Logs "Hello, World!"
+//   count++; // Increments count by 1
+// }
+// // Repeats logging "Hello, World!" 5 times, once per loop iteration.
+
+
+//task 120
+
+// // Defines an array of favorite movies
+// const favoriteMovies: string[] = [
+//     "Inception",
+//     "The Matrix",
+//     "Interstellar",
+//     "The Prestige",
+//   ];
+//   // Uses a for...of loop to iterate through favoriteMovies
+//   for (const movie of favoriteMovies) {
+//     console.log(movie); // Logs each movie name
+//   }
+//   // Each movie in the array is logged to the console, one at a time.
